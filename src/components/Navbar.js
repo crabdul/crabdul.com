ed466309549316ee80b4a6c286b15a2cb6ae4727
crabdul <23074754+crabdul@users.noreply.github.com>
2019-01-06 13:07:40 +0000

    Remove unused links from navbar and add active link styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,64 +1,26 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import logo from '../img/logo.svg'
 
 const Navbar = class extends React.Component {
 
-  componentDidMount() {
-    // Get all "navbar-burger" elements
-   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-    // Check if there are any navbar burgers
-   if ($navbarBurgers.length > 0) {
-
-     // Add a click event on each of them
-     $navbarBurgers.forEach( el => {
-       el.addEventListener('click', () => {
-
-         // Get the target from the "data-target" attribute
-         const target = el.dataset.target;
-         const $target = document.getElementById(target);
-
-         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-         el.classList.toggle('is-active');
-         $target.classList.toggle('is-active');
-
-       });
-     });
-   }
- }
 
  render() {
+	const activeLinkStyle = {
+		color: "rgb(233, 30, 99)",
+	}
    return (
 
   <nav className="navbar is-transparent" role="navigation" aria-label="main-navigation">
     <div className="container">
-      <div className="navbar-brand">
         <Link to="/" className="navbar-item" title="Logo">
-          <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            crab.land
         </Link>
-        {/* Hamburger menu */}
-        <div className="navbar-burger burger" data-target="navMenu">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </div>
-      <div id="navMenu" className="navbar-menu">
-      <div className="navbar-start has-text-centered">
-        <Link className="navbar-item" to="/about">
+        <Link className="navbar-item" to="/about" activeStyle={activeLinkStyle}>
           About
         </Link>
-        <Link className="navbar-item" to="/products">
-          Products
+        <Link className="navbar-item" to="/" activeStyle={activeLinkStyle}>
+          Photos
         </Link>
-        <Link className="navbar-item" to="/contact">
-          Contact
-        </Link>
-        <Link className="navbar-item" to="/contact/examples">
-          Form Examples
-        </Link>
-      </div>
-      <div className="navbar-end has-text-centered">
         <a
           className="navbar-item"
           href="https://elkhazaani.space/"
@@ -68,8 +30,6 @@ const Navbar = class extends React.Component {
             elkhazaani.space
         </a>
       </div>
-      </div>
-    </div>
   </nav>
   )}
 }
