659beacc01087e49a872e737c5e1e272b4634151
crabdul <23074754+crabdul@users.noreply.github.com>
2019-01-13 17:13:06 +0000

    Add date last updated to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,21 @@
 import React from 'react'
 
-const Footer = () => (
-    <footer className="container">
-        <div className="footer">
-            <p>
-                <a href="https://github.com/crabdul">crabdul</a> /{' '}
-                <a href="mailto:karim.elkhazaani@zoho.com">
-                    karim.elkhazaani@zoho.com
-                </a>
-            </p>
-        </div>
-    </footer>
-)
+const Footer = () => {
+    const date = new Date().toString().split(' ').slice(1,4).join(' ')
+    return (
+        <footer className="container">
+            <div className="footer">
+                <p>
+                    <a href="https://github.com/crabdul">crabdul</a> /{' '}
+                    <a href="mailto:karim.elkhazaani@zoho.com">
+                        karim.elkhazaani@zoho.com
+                    </a>
+                </p>
+                <p>
+                    Last Update: <small>{date}</small>
+                </p>
+            </div>
+        </footer>
+    )}
 
 export default Footer
