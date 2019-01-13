a847d3c6d46fe252cd48b5c84aa53a203b832aa3
crabdul <23074754+crabdul@users.noreply.github.com>
2019-01-13 16:12:29 +0000

    Show paginated posts at the botto of blog post

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,10 +3,22 @@ const path = require('path')
 const { createFilePath } = require('gatsby-source-filesystem')
 const { fmImagesToRelative } = require('gatsby-remark-relative-images')
 
-exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions
+const findPreviousPostId = (posts, index) => {
+    const post = posts.find((post) => post.node.frontmatter.templateKey == 'blog-post')
+    if (post) {
+        return post.node.id
+    }
+    return null
+}
 
-  return graphql(`
+const findNextPostId = (posts, index) => {
+    return findPreviousPostId(posts.reverse(), index)
+}
+
+exports.createPages = ({ actions, graphql }) => {
+    const { createPage } = actions
+
+    return graphql(`
     {
       allMarkdownRemark(limit: 1000) {
         edges {
@@ -24,64 +36,72 @@ exports.createPages = ({ actions, graphql }) => {
       }
     }
   `).then(result => {
-    if (result.errors) {
-      result.errors.forEach(e => console.error(e.toString()))
-      return Promise.reject(result.errors)
-    }
-
-    const posts = result.data.allMarkdownRemark.edges
-
-    posts.forEach(edge => {
-      const id = edge.node.id
-      createPage({
-        path: edge.node.fields.slug,
-        tags: edge.node.frontmatter.tags,
-        component: path.resolve(
-          `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
-        ),
-        // additional data can be passed via context
-        context: {
-          id,
-        },
-      })
-    })
-
-    // Tag pages:
-    let tags = []
-    // Iterate through each post, putting all found tags into `tags`
-    posts.forEach(edge => {
-      if (_.get(edge, `node.frontmatter.tags`)) {
-        tags = tags.concat(edge.node.frontmatter.tags)
+      if (result.errors) {
+          result.errors.forEach(e => console.error(e.toString()))
+          return Promise.reject(result.errors)
       }
-    })
-    // Eliminate duplicate tags
-    tags = _.uniq(tags)
 
-    // Make tag pages
-    tags.forEach(tag => {
-      const tagPath = `/tags/${_.kebabCase(tag)}/`
+      const posts = result.data.allMarkdownRemark.edges
 
-      createPage({
-        path: tagPath,
-        component: path.resolve(`src/templates/tags.js`),
-        context: {
-          tag,
-        },
+      posts.forEach((edge, index) => {
+          const id = edge.node.id
+
+          const olderPosts = posts.slice(index + 1)
+          const newerPosts = posts.slice(0, index)
+
+          const previousId = index === 0 || olderPosts.length === 0 ? null : findPreviousPostId(olderPosts, index)
+          const nextId = index === posts.length - 1 && newerPosts.length === 0 ? null : findNextPostId(newerPosts.reverse(), index)
+          createPage({
+              path: edge.node.fields.slug,
+              tags: edge.node.frontmatter.tags,
+              component: path.resolve(
+                  `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
+              ),
+              // additional data can be passed via context
+              context: {
+                  id,
+                  nextId,
+                  previousId,
+              },
+          })
       })
-    })
+
+      // Tag pages:
+      let tags = []
+      // Iterate through each post, putting all found tags into `tags`
+      posts.forEach(edge => {
+          if (_.get(edge, `node.frontmatter.tags`)) {
+              tags = tags.concat(edge.node.frontmatter.tags)
+          }
+      })
+      // Eliminate duplicate tags
+      tags = _.uniq(tags)
+
+      // Make tag pages
+      tags.forEach(tag => {
+          const tagPath = `/tags/${_.kebabCase(tag)}/`
+
+          createPage({
+              path: tagPath,
+              component: path.resolve(`src/templates/tags.js`),
+              context: {
+                  tag,
+              },
+          })
+      })
   })
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
-  fmImagesToRelative(node) // convert image paths for gatsby images
+    const { createNodeField } = actions
+    fmImagesToRelative(node) // convert image paths for gatsby images
 
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    })
-  }
+    if (node.internal.type === `MarkdownRemark`) {
+        const value = createFilePath({ node, getNode })
+        createNodeField({
+            name: `slug`,
+            node,
+            value,
+        })
+    }
 }
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,102 +3,175 @@ import PropTypes from 'prop-types'
 import { kebabCase } from 'lodash'
 import Helmet from 'react-helmet'
 import { graphql, Link } from 'gatsby'
+import Entry from '../components/Entry'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
+import { animateAndNavigateTo } from '../utils/helpers'
 
 export const BlogPostTemplate = ({
-  content,
-  contentComponent,
-  date,
-  description,
-  tags,
-  title,
-  helmet,
+    content,
+    contentComponent,
+    date,
+    description,
+    tags,
+    title,
+    helmet,
 }) => {
-  const PostContent = contentComponent || Content
+    const PostContent = contentComponent || Content
 
-  return (
-    <section className="section blog-post">
-      {helmet || ''}
-      <div className="container">
-           <div className="post-meta-content">
-                <div className="post-meta">
-                    <h1 className="p">{title}</h1>
-                    <small>{date}</small>
+    return (
+        <section className="section blog-post">
+            {helmet || ''}
+            <div className="container">
+                <div className="post-meta-content">
+                    <div className="post-meta">
+                        <h1 className="p">{title}</h1>
+                        <small>{date}</small>
+                    </div>
+                    <div className="post-content">
+                        <PostContent content={content} title={title} />
+                        {tags && tags.length ? (
+                            <div
+                                style={{
+                                    marginTop: `4rem`,
+                                    marginBottom: `2rem`,
+                                }}
+                            >
+                                <h4>Tags</h4>
+                                <ul className="taglist">
+                                    {tags.map(tag => (
+                                        <li key={tag + `tag`}>
+                                            <Link
+                                                to={`/tags/${kebabCase(tag)}/`}
+                                            >
+                                                {tag}
+                                            </Link>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        ) : null}
+                    </div>
                 </div>
-                <div className="post-content">
-                    <PostContent content={content} title={title} />
-                    {tags && tags.length ? (
-                        <div style={{ marginTop: `4rem`, marginBottom: `2rem` }}>
-                            <h4>Tags</h4>
-                            <ul className="taglist">
-                                {tags.map(tag => (
-                                    <li key={tag + `tag`}>
-                                        <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    ) : null}
-                </div>
-           </div>
-      </div>
-    </section>
-  )
+            </div>
+        </section>
+    )
 }
 
 BlogPostTemplate.propTypes = {
-  content: PropTypes.node.isRequired,
-  contentComponent: PropTypes.func,
-  description: PropTypes.string,
-  title: PropTypes.string,
-  helmet: PropTypes.object,
+    content: PropTypes.node.isRequired,
+    contentComponent: PropTypes.func,
+    date: PropTypes.string,
+    description: PropTypes.string,
+    tags: PropTypes.array,
+    title: PropTypes.string,
+    helmet: PropTypes.object,
 }
 
 const BlogPost = ({ data }) => {
-  const { markdownRemark: post } = data
+    const { post } = data
+    const { nextPost, previousPost } = data
+    const posts = []
+    if (nextPost) {
+        posts.push(nextPost)
+    }
+    if (previousPost) {
+        posts.push(previousPost)
+    }
 
-  return (
-    <Layout>
-      <BlogPostTemplate
-        content={post.html}
-        contentComponent={HTMLContent}
-        date={post.frontmatter.date}
-        description={post.frontmatter.description}
-        helmet={
-          <Helmet
-            titleTemplate="%s | Blog"
-          >
-            <title>{`${post.frontmatter.title}`}</title>
-            <meta name="description" content={`${post.frontmatter.description}`} />
-          </Helmet>
-        }
-        tags={post.frontmatter.tags}
-        title={post.frontmatter.title}
-      />
-    </Layout>
-  )
+    return (
+        <Layout>
+            <BlogPostTemplate
+                content={post.html}
+                contentComponent={HTMLContent}
+                date={post.frontmatter.date}
+                description={post.frontmatter.description}
+                helmet={
+                    <Helmet titleTemplate="%s | Blog">
+                        <title>{`${post.frontmatter.title}`}</title>
+                        <meta
+                            name="description"
+                            content={`${post.frontmatter.description}`}
+                        />
+                    </Helmet>
+                }
+                tags={post.frontmatter.tags}
+                title={post.frontmatter.title}
+            />
+            <div className="container">
+                <ul>
+                    {posts.map((post, i) => (
+                        <li key={i}>
+                            {i == 0 && <div className="black-line" />}
+                            <Entry
+                                post={post}
+                                animateAndNavigateTo={animateAndNavigateTo}
+                            />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </Layout>
+    )
 }
 
 BlogPost.propTypes = {
-  data: PropTypes.shape({
-    markdownRemark: PropTypes.object,
-  }),
+    data: PropTypes.shape({
+        markdownRemark: PropTypes.object,
+    }),
 }
 
 export default BlogPost
 
 export const pageQuery = graphql`
-  query BlogPostByID($id: String!) {
-    markdownRemark(id: { eq: $id }) {
-      id
-      html
-      frontmatter {
-        date(formatString: "MMMM DD, YYYY")
-        title
-        description
-        tags
-      }
+    query BlogPostByID($id: String!, $nextId: String, $previousId: String) {
+        post: markdownRemark(id: { eq: $id }) {
+            id
+            html
+            frontmatter {
+                date(formatString: "MMMM DD, YYYY")
+                title
+                description
+                tags
+            }
+        }
+        nextPost: markdownRemark(id: { eq: $nextId }) {
+            excerpt(pruneLength: 400)
+            id
+            fields {
+                slug
+            }
+            frontmatter {
+                coverPhoto {
+                    childImageSharp {
+                        sizes(maxWidth: 600) {
+                            ...GatsbyImageSharpSizes
+                        }
+                    }
+                }
+                title
+                templateKey
+                date(formatString: "MMMM DD, YYYY")
+            }
+        }
+        previousPost: markdownRemark(id: { eq: $previousId }) {
+            excerpt(pruneLength: 400)
+            id
+            fields {
+                slug
+            }
+            frontmatter {
+                coverPhoto {
+                    childImageSharp {
+                        sizes(maxWidth: 600) {
+                            ...GatsbyImageSharpSizes
+                        }
+                    }
+                }
+                title
+                templateKey
+                date(formatString: "MMMM DD, YYYY")
+            }
+        }
     }
-  }
 `
