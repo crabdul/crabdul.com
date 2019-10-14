5535b1545597c5748fae18c5d94c3a71d8a65245
crabdul <23074754+crabdul@users.noreply.github.com>
2019-10-15 00:21:47 +0100

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-    const date = new Date().toString().split(' ').slice(1,4).join(' ')
-    return (
-        <footer className="container">
-            <div className="footer">
-                <p>
-                    <a href="https://github.com/crabdul">Github: crabdul</a> /{' '}
-                    <a href="mailto:karim.elkhazaani@zoho.com">
-                        Email
-                    </a>
-                </p>
-                <p>
-                    Last Update: <small>{date}</small>
-                </p>
-            </div>
-        </footer>
-    )}
+  const date = new Date().getFullYear();
+  return (
+    <footer className="container">
+      <div className="footer">
+        <p>
+          <a href="https://github.com/crabdul" target="_blank">
+            Github: crabdul
+          </a>{" "}
+          /{" "}
+          <a href="mailto:karim.elkhazaani@zoho.com" target="_blank">
+            Email
+          </a>
+          /{" "}
+          <a href="https://www.linkedin.com/in/el-khazaani/" target="_blank">
+            LinkedIn
+          </a>
+        </p>
+        <p>
+          Copyright <small>{date}</small>
+        </p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
