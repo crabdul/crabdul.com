d4efbc25e6f82d889371b3927ca9659efa5090af
crabdul <23074754+crabdul@users.noreply.github.com>
2021-02-13 11:27:14 +0000

    Dont show image on entry

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Img from 'gatsby-image';
 import { animateAndNavigateTo } from '../utils/helpers';
 
 const Entry = ({ post }) => (
@@ -13,11 +12,6 @@ const Entry = ({ post }) => (
             <small className="text-gray-800">{post.frontmatter.date}</small>
         </div>
         <div className="post-content">
-            <Img
-                className="mb-5 post-content"
-                sizes={post.frontmatter.coverPhoto.childImageSharp.sizes}
-                alt={post.frontmatter.title}
-            />
             <p className="prose">{post.excerpt}</p>
         </div>
     </article>
