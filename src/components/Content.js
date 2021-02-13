16eedec721e5084b3237af33241c93e4657ffca9
crabdul <23074754+crabdul@users.noreply.github.com>
2021-02-13 09:45:30 +0000

    Use prose styling

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,19 +1,26 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 export const HTMLContent = ({ content, className }) => (
-    <div className={className} dangerouslySetInnerHTML={{ __html: content }} />
-)
+    <div class="prose">
+        <div
+            className={className}
+            dangerouslySetInnerHTML={{ __html: content }}
+        />
+    </div>
+);
 
 const Content = ({ content, className }) => (
-    <div className={className}>{content}</div>
-)
+    <div class="prose">
+        <div className={className}>{content}</div>
+    </div>
+);
 
 Content.propTypes = {
     content: PropTypes.node,
-    className: PropTypes.string,
-}
+    className: PropTypes.string
+};
 
-HTMLContent.propTypes = Content.propTypes
+HTMLContent.propTypes = Content.propTypes;
 
-export default Content
+export default Content;
--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -9,16 +9,16 @@ const Entry = ({ post }) => (
         key={post.id}
     >
         <div className="post-meta">
-            <h2 className="p">{post.frontmatter.title}</h2>
-            <small>{post.frontmatter.date}</small>
+            <h2 className="p font-bold text-lg">{post.frontmatter.title}</h2>
+            <small className="text-gray-800">{post.frontmatter.date}</small>
         </div>
         <div className="post-content">
             <Img
-                className="post-content"
+                className="mb-5 post-content"
                 sizes={post.frontmatter.coverPhoto.childImageSharp.sizes}
                 alt={post.frontmatter.title}
             />
-            <p>{post.excerpt}</p>
+            <p className="prose">{post.excerpt}</p>
         </div>
     </article>
 );
