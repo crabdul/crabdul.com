be6ad71dff6fcf9848f6d2c392d7d5331f8681ae
crabdul <23074754+crabdul@users.noreply.github.com>
2021-02-13 10:09:12 +0000

    Rename className

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const HTMLContent = ({ content, className }) => (
-    <div class="prose">
+    <div className="prose">
         <div
             className={className}
             dangerouslySetInnerHTML={{ __html: content }}
@@ -11,7 +11,7 @@ export const HTMLContent = ({ content, className }) => (
 );
 
 const Content = ({ content, className }) => (
-    <div class="prose">
+    <div className="prose">
         <div className={className}>{content}</div>
     </div>
 );
